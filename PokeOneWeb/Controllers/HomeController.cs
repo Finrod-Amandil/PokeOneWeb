3ede16cffb0507fb4455b4bc9c05c893c33eeb75
Finrod_Amandil <severin.zahler@gmail.com>
2020-09-07 08:56:56 +0200

    PokeApiService

--- PokeOneWeb/Controllers/HomeController.cs
+++ PokeOneWeb/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PokeOneWeb.Data;
 using PokeOneWeb.Models;
+using PokeOneWeb.Services.PokeApi;
 
 namespace PokeOneWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPokeApiService _pokeApiService;
+        private readonly ApplicationDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IPokeApiService pokeApiService,
+            ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _pokeApiService = pokeApiService;
+            _dbContext = dbContext;
         }
 
         public IActionResult Index()
@@ -32,6 +40,28 @@ namespace PokeOneWeb.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> GetData()
+        {
+            return Ok();
+
+            var data = await _pokeApiService.DownloadData();
+
+            _dbContext.PokemonSpecies.AddRange(data.PokemonSpecies.Values);
+            _dbContext.Moves.AddRange(data.Moves.Values);
+            _dbContext.Items.AddRange(data.Items.Values);
+
+            try
+            {
+                _dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+
+            return Ok();
         }
     }
 }
---
+++ PokeOneWeb/Services/PokeApi/Impl/PokeApiService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using PokeOneWeb.Configuration;
+using System.Threading.Tasks;
+
+namespace PokeOneWeb.Services.PokeApi.Impl
+{
+    public class PokeApiService : IPokeApiService
+    {
+        private readonly PokeApiLoader _loader;
+        private readonly PokeApiMapper _mapper;
+
+        public PokeApiService(IOptions<PokeApiSettings> pokeApiSettings)
+        {
+            _loader = new PokeApiLoader(pokeApiSettings);
+            _mapper = new PokeApiMapper(pokeApiSettings);
+        }
+
+        public async Task<MappedPokeApiData> DownloadData()
+        {
+            var data = await _loader.LoadPokeApiData();
+
+            var mappedData = _mapper.MapPokeApiData(data);
+
+            return mappedData;
+        }
+    }
+}
