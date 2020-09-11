a63782238c804d069396489d045180be5ad4f67f
Finrod_Amandil <severin.zahler@gmail.com>
2020-09-11 09:08:44 +0200

    Google Spreadsheet Mappers

---
+++ PokeOneWeb/Data/Migrations/20200910105045_placeditem_isconfirmed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PokeOneWeb.Data.Migrations
+{
+    public partial class placeditem_isconfirmed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfirmed",
+                table: "PlacedItem",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfirmed",
+                table: "PlacedItem");
+        }
+    }
+}
