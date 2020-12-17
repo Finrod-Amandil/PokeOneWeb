587b0e648c2b68997675c7c703bf94fa3f4082a1
Finrod_Amandil <severin.zahler@gmail.com>
2020-12-17 07:37:01 +0100

    Pokémon List

---
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/common/string.constants.ts
@@ -0,0 +1,2 @@
+export var SELECT_OPTION_ANY = 'Any';
+export var SELECT_OPTION_NONE = 'None';
---
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/common/url.constants.ts
@@ -0,0 +1,6 @@
+export var SMOGON_BASE_URL = 'https://www.smogon.com/dex/sm/pokemon/';
+export var BULBAPEDIA_BASE_URL = 'https://bulbapedia.bulbagarden.net/wiki/';
+export var POKEONECOMMUNITY_BASE_URL = 'https://www.pokeonecommunity.com/index.php?mod=pokemon&action=pokemon&id=';
+export var POKEMONSHOWDOWN_BASE_URL = 'https://dex.pokemonshowdown.com/pokemon/';
+export var SEREBII_BASE_URL = 'https://www.serebii.net/pokedex-sm/';
+export var POKEMONDB_BASE_URL = 'https://pokemondb.net/pokedex/';
