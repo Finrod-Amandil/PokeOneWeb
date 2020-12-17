901b5b7824f553026f1364b1915f654c4c551bfa
Finrod_Amandil <severin.zahler@gmail.com>
2020-12-17 07:36:21 +0100

    navbar

---
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/shared/navbar-item/navbar-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'pokeone-navbar-item',
+  templateUrl: './navbar-item.component.html',
+  styleUrls: ['./navbar-item.component.scss']
+})
+export class NavbarItemComponent implements OnInit {
+  @Input() menuItemCaption: string = "menu-item";
+  @Input() menuItemImage: string = "#";
+  @Input() subMenuItems: string[] = [];
+
+  ngOnInit(): void {
+  }
+
+}
