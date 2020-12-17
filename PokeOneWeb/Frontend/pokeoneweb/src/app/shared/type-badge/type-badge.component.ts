70bf0f14d5a03fe6e33e5d98a76146a0e266ca1e
Finrod_Amandil <severin.zahler@gmail.com>
2020-12-17 07:36:07 +0100

    type badge component

---
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/shared/type-badge/type-badge.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'pokeone-type-badge',
+  templateUrl: './type-badge.component.html',
+  styleUrls: ['./type-badge.component.scss']
+})
+export class TypeBadgeComponent implements OnInit {
+
+  @Input() typeName: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
