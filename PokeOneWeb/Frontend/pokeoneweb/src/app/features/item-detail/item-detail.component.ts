7f94e173cb8476d327d861fb7af37f26f27c7dc7
Finrod_Amandil <severin.zahler@gmail.com>
2020-12-25 10:21:56 +0100

    stubs for item, location and pokemon pages

--- PokeOneWeb/Frontend/pokeoneweb/src/app/features/item-detail/item-detail.component.ts
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/features/item-detail/item-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemDetailComponent implements OnInit {
 
-  constructor() { }
+  public itemName: string = '';
+
+  constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(result => this.itemName = result["resourceName"]);
   }
 
 }
