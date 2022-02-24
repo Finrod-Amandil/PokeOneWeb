f28c1506a8deed95a2da799618b3d40e8be8c852
Finrod_Amandil <severin.zahler@gmail.com>
2022-02-24 21:50:01 +0100

    Show priority in list of learnable moves

--- PokeOneWeb/Frontend/pokeoneweb/src/app/pages/pokemon-detail/core/pokemon-detail-sort.service.ts
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/pages/pokemon-detail/core/pokemon-detail-sort.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from "@angular/core";
-import { ILearnableMoveModel } from "src/app/core/models/learnable-move.model";
-import { ISpawnModel } from "src/app/core/models/spawn.model";
-import { MoveListColumn } from "./move-list-column.enum";
-import { SpawnListColumn } from "./spawn-list-column.enum";
+import { Injectable } from '@angular/core';
+import { ILearnableMoveModel } from 'src/app/core/models/learnable-move.model';
+import { ISpawnModel } from 'src/app/core/models/spawn.model';
+import { MoveListColumn } from './move-list-column.enum';
+import { SpawnListColumn } from './spawn-list-column.enum';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class PokemonDetailSortService {
-    public sortSpawns(models: ISpawnModel[], sortColumn: SpawnListColumn, sortDirection: number): ISpawnModel[] {
+    public sortSpawns(
+        models: ISpawnModel[],
+        sortColumn: SpawnListColumn,
+        sortDirection: number
+    ): ISpawnModel[] {
         switch (sortColumn) {
             case SpawnListColumn.Pokemon:
                 return this.sortSpawnsByPokemon(models, sortDirection);
@@ -23,7 +27,11 @@ export class PokemonDetailSortService {
         return models;
     }
 
-    public sortMoves(models: ILearnableMoveModel[], sortColumn: MoveListColumn, sortDirection: number): ILearnableMoveModel[] {
+    public sortMoves(
+        models: ILearnableMoveModel[],
+        sortColumn: MoveListColumn,
+        sortDirection: number
+    ): ILearnableMoveModel[] {
         switch (sortColumn) {
             case MoveListColumn.Name:
                 return this.sortMovesByName(models, sortDirection);
@@ -37,89 +45,157 @@ export class PokemonDetailSortService {
                 return this.sortMovesByAccuracy(models, sortDirection);
             case MoveListColumn.PowerPoints:
                 return this.sortMovesByPowerPoints(models, sortDirection);
+            case MoveListColumn.Priority:
+                return this.sortMovesByPriority(models, sortDirection);
         }
 
         return models;
     }
 
-    private sortSpawnsByPokemon(models: ISpawnModel[], sortDirection: number): ISpawnModel[] {
-        return models.slice().sort((n1, n2) => sortDirection * (n1.pokemonFormSortIndex - n2.pokemonFormSortIndex));
+    private sortSpawnsByPokemon(
+        models: ISpawnModel[],
+        sortDirection: number
+    ): ISpawnModel[] {
+        return models
+            .slice()
+            .sort(
+                (n1, n2) =>
+                    sortDirection *
+                    (n1.pokemonFormSortIndex - n2.pokemonFormSortIndex)
+            );
     }
 
-    private sortSpawnsByLocation(models: ISpawnModel[], sortDirection: number): ISpawnModel[] {
-        return models.slice().sort((n1, n2) => sortDirection * (n1.locationSortIndex - n2.locationSortIndex));
+    private sortSpawnsByLocation(
+        models: ISpawnModel[],
+        sortDirection: number
+    ): ISpawnModel[] {
+        return models
+            .slice()
+            .sort(
+                (n1, n2) =>
+                    sortDirection *
+                    (n1.locationSortIndex - n2.locationSortIndex)
+            );
     }
 
-    private sortSpawnsBySpawnType(models: ISpawnModel[], sortDirection: number): ISpawnModel[] {
-        return models.slice().sort((n1, n2) => sortDirection * (n1.spawnTypeSortIndex - n2.spawnTypeSortIndex));
+    private sortSpawnsBySpawnType(
+        models: ISpawnModel[],
+        sortDirection: number
+    ): ISpawnModel[] {
+        return models
+            .slice()
+            .sort(
+                (n1, n2) =>
+                    sortDirection *
+                    (n1.spawnTypeSortIndex - n2.spawnTypeSortIndex)
+            );
     }
 
-    private sortSpawnsByRarity(models: ISpawnModel[], sortDirection: number): ISpawnModel[] {
-        return models.slice()
-            .sort((n1, n2) => {
-                if (n1.rarityValue !== n2.rarityValue) {
-                    return sortDirection * (n2.rarityValue - n1.rarityValue);
-                }
-                else {
-                    return n1.rarityString > n2.rarityString ? sortDirection * 1 : sortDirection * -1;
-                }
-            });
+    private sortSpawnsByRarity(
+        models: ISpawnModel[],
+        sortDirection: number
+    ): ISpawnModel[] {
+        return models.slice().sort((n1, n2) => {
+            if (n1.rarityValue !== n2.rarityValue) {
+                return sortDirection * (n2.rarityValue - n1.rarityValue);
+            } else {
+                return n1.rarityString > n2.rarityString
+                    ? sortDirection * 1
+                    : sortDirection * -1;
+            }
+        });
     }
 
-    private sortMovesByName(models: ILearnableMoveModel[], sortDirection: number): ILearnableMoveModel[] {
-        return models.slice()
-            .sort((n1, n2) => {
-                if (n1.moveName > n2.moveName) {
-                    return sortDirection * 1;
-                }
+    private sortMovesByName(
+        models: ILearnableMoveModel[],
+        sortDirection: number
+    ): ILearnableMoveModel[] {
+        return models.slice().sort((n1, n2) => {
+            if (n1.moveName > n2.moveName) {
+                return sortDirection * 1;
+            }
 
-                if (n1.moveName < n2.moveName) {
-                    return sortDirection * -1;
-                }
+            if (n1.moveName < n2.moveName) {
+                return sortDirection * -1;
+            }
 
-                return 0;
-            });
+            return 0;
+        });
     }
 
-    private sortMovesByType(models: ILearnableMoveModel[], sortDirection: number): ILearnableMoveModel[] {
-        return models.slice()
-            .sort((n1, n2) => {
-                if (n1.elementalType > n2.elementalType) {
-                    return sortDirection * 1;
-                }
+    private sortMovesByType(
+        models: ILearnableMoveModel[],
+        sortDirection: number
+    ): ILearnableMoveModel[] {
+        return models.slice().sort((n1, n2) => {
+            if (n1.elementalType > n2.elementalType) {
+                return sortDirection * 1;
+            }
 
-                if (n1.elementalType < n2.elementalType) {
-                    return sortDirection * -1;
-                }
+            if (n1.elementalType < n2.elementalType) {
+                return sortDirection * -1;
+            }
 
-                return 0;
-            });
+            return 0;
+        });
     }
 
-    private sortMovesByClass(models: ILearnableMoveModel[], sortDirection: number): ILearnableMoveModel[] {
-        return models.slice()
-            .sort((n1, n2) => {
-                if (n1.damageClass > n2.damageClass) {
-                    return sortDirection * 1;
-                }
+    private sortMovesByClass(
+        models: ILearnableMoveModel[],
+        sortDirection: number
+    ): ILearnableMoveModel[] {
+        return models.slice().sort((n1, n2) => {
+            if (n1.damageClass > n2.damageClass) {
+                return sortDirection * 1;
+            }
 
-                if (n1.damageClass < n2.damageClass) {
-                    return sortDirection * -1;
-                }
+            if (n1.damageClass < n2.damageClass) {
+                return sortDirection * -1;
+            }
 
-                return 0;
-            });
+            return 0;
+        });
     }
 
-    private sortMovesByPower(models: ILearnableMoveModel[], sortDirection: number): ILearnableMoveModel[] {
-        return models.slice().sort((n1, n2) => sortDirection * (n2.effectiveAttackPower - n1.effectiveAttackPower));
+    private sortMovesByPower(
+        models: ILearnableMoveModel[],
+        sortDirection: number
+    ): ILearnableMoveModel[] {
+        return models
+            .slice()
+            .sort(
+                (n1, n2) =>
+                    sortDirection *
+                    (n2.effectiveAttackPower - n1.effectiveAttackPower)
+            );
     }
 
-    private sortMovesByAccuracy(models: ILearnableMoveModel[], sortDirection: number): ILearnableMoveModel[] {
-        return models.slice().sort((n1, n2) => sortDirection * (n2.accuracy - n1.accuracy));
+    private sortMovesByAccuracy(
+        models: ILearnableMoveModel[],
+        sortDirection: number
+    ): ILearnableMoveModel[] {
+        return models
+            .slice()
+            .sort((n1, n2) => sortDirection * (n2.accuracy - n1.accuracy));
     }
 
-    private sortMovesByPowerPoints(models: ILearnableMoveModel[], sortDirection: number): ILearnableMoveModel[] {
-        return models.slice().sort((n1, n2) => sortDirection * (n2.powerPoints - n1.powerPoints));
+    private sortMovesByPowerPoints(
+        models: ILearnableMoveModel[],
+        sortDirection: number
+    ): ILearnableMoveModel[] {
+        return models
+            .slice()
+            .sort(
+                (n1, n2) => sortDirection * (n2.powerPoints - n1.powerPoints)
+            );
+    }
+
+    private sortMovesByPriority(
+        models: ILearnableMoveModel[],
+        sortDirection: number
+    ): ILearnableMoveModel[] {
+        return models
+            .slice()
+            .sort((n1, n2) => sortDirection * (n2.priority - n1.priority));
     }
 }
