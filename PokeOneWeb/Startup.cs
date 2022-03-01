c69c28388c7e864f403c574396a20acb2149b975
Finrod_Amandil <severin.zahler@gmail.com>
2022-03-01 16:40:44 +0100

    Create dummy file to ensure wwwroot folder is created.

--- PokeOneWeb/Startup.cs
+++ PokeOneWeb/Startup.cs
@@ -59,7 +59,6 @@ namespace PokeOneWeb.WebApi
             }
             else
             {
-                app.UseExceptionHandler("/");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
