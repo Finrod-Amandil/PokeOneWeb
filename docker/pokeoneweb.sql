5c16c08546aafef82bf56680e39918ed99e81509
bischraf <bischraf@students.zhaw.ch>
2022-03-05 19:35:27 +0100

    removed wrong sql file

---
+++ docker/pokeoneweb.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:83811b12d1b46e6049df57f7989729a8b6ca10d223400885b719e8e15c6be324
+size 113697530
--- docker/readModelDbData.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6dea100b39d14eba3205dda8c0923633e59b7153a7bfeb362cc0b86688dc2aa9
-size 239623900
