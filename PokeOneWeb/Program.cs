c6448a9b160f2b2a9dbaa8acabdb97edc9a41b09
bischraf <bischraf@students.zhaw.ch>
2022-03-05 11:23:31 +0100

    added first iteration of docker. SQL container working, dotnet container not yet

--- PokeOneWeb/Program.cs
+++ PokeOneWeb/Program.cs
@@ -14,7 +14,7 @@ namespace PokeOneWeb.WebApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://+:5000");
                 });
     }
 }
