0263afe4f7db814aabe9bb30ab7fe91f20f8b4df
bischraf <bischraf@students.zhaw.ch>
2022-03-05 11:23:31 +0100

    added first iteration of docker. SQL container working, dotnet container not yet

---
+++ docker/db-init.sh
@@ -0,0 +1,16 @@
+#wait for the SQL Server to come up
+for i in {1..50};
+do
+    echo "running db generation"
+    #run the setup script to create the DB 
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure*Password1 -d master -i docker/pokeonewebreadmodel.sql
+    if [ $? -eq 0 ]
+    then
+        /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure*Password1 -d master -i docker/pokeoneweb.sql
+        echo "finished db generation"
+        break
+    else
+        echo "not ready yet..."
+        sleep 1
+    fi
+done
