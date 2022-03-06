b7be3f80491e7391bf459f31536b5dd2997e8f60
Bischi1997 <rafael.bischofberger@gmail.com>
2022-03-06 12:50:08 +0100

    updated db creation script, to check if database exists

--- docker/db/db-init.sh
+++ docker/db/db-init.sh
@@ -1,16 +1,39 @@
-#wait for the SQL Server to come up
-for i in {1..50};
-do
-    echo "running db generation"
-    #run the setup script to create the DB 
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure*Password1 -d master -i db/pokeonewebreadmodel.sql
-    if [ $? -eq 0 ]
-    then
-        /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure*Password1 -d master -i db/pokeoneweb.sql
-        echo "finished db generation"
-        break
-    else
-        echo "not ready yet..."
-        sleep 1
-    fi
-done
+#!/bin/bash
+#check if db files are already existing
+if [[ -f /var/opt/mssql/data/PokeOneWeb.mdf ]] && [[ -f /var/opt/mssql/data/PokeOneWebReadModel.mdf ]]
+then
+    echo "#####################################################"
+    echo "Databases both already exist. Skipping db generation"
+    echo "#####################################################"
+else
+    #wait for the SQL Server to come up (max ~60s)
+    for i in {1..60};
+    do
+        /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure*Password1 -Q "SELECT @@VERSION"
+        if [ $? -eq 0 ]
+        then
+            echo "#####################################################"
+            echo "running db generation"
+            start=`date +%s`
+            #run the setup script to create the DB and import its data
+            if [[ ! -f /var/opt/mssql/data/PokeOneWebReadModel.mdf ]]
+            then
+                echo "DB PokeOneWebReadModel is not existing, starting creation..."
+                /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure*Password1 -d master -i db/pokeonewebreadmodel.sql
+            fi
+            if [[ ! -f /var/opt/mssql/data/PokeOneWeb.mdf ]]
+            then
+                echo "DB PokeOneWeb is not existing, starting creation..."
+                /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure*Password1 -d master -i db/pokeoneweb.sql
+            fi
+            end=`date +%s`
+            runtime=$((end-start))
+            echo "finished db generation in $runtime s"
+            echo "#####################################################"
+            break
+        else
+            echo "not ready yet..."
+            sleep 1
+        fi
+    done
+fi
