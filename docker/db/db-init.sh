a0537857d347db06bfd18bc9890651bcb2aa7af3
ryserrja <ryserrja@students.zhaw.ch>
2022-03-10 14:58:34 +0100

    changed from bash to sh cause bash somehow does not work

--- docker/db/db-init.sh
+++ docker/db/db-init.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 #check if db files are already existing
-if [[ -f /var/opt/mssql/data/PokeOneWeb.mdf ]] && [[ -f /var/opt/mssql/data/PokeOneWebReadModel.mdf ]]
+if [ -f /var/opt/mssql/data/PokeOneWeb.mdf ] && [ -f /var/opt/mssql/data/PokeOneWebReadModel.mdf ]
 then
     echo "#####################################################"
     echo "Databases both already exist. Skipping db generation"
     echo "#####################################################"
 else
     #wait for the SQL Server to come up (max ~60s)
-    for i in {1..60};
+    i=1
+    j=1
+    while  [ $i -le 60 ]
     do
         /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "SELECT @@VERSION"
         if [ $? -eq 0 ]
@@ -16,15 +18,15 @@ else
             echo "running db generation"
             start=`date +%s`
             #run the setup script to create the DB and import its data
-            if [[ ! -f /var/opt/mssql/data/PokeOneWebReadModel.mdf ]]
+            if [ ! -f /var/opt/mssql/data/PokeOneWebReadModel.mdf ]
             then
                 echo "DB PokeOneWebReadModel is not existing, starting creation..."
-                /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i db/pokeonewebreadmodel.sql
+                /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /db/pokeonewebreadmodel.sql
             fi
-            if [[ ! -f /var/opt/mssql/data/PokeOneWeb.mdf ]]
+            if [ ! -f /var/opt/mssql/data/PokeOneWeb.mdf ]
             then
                 echo "DB PokeOneWeb is not existing, starting creation..."
-                /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i db/pokeoneweb.sql
+                /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /db/pokeoneweb.sql
             fi
             end=`date +%s`
             runtime=$((end-start))
@@ -35,5 +37,6 @@ else
             echo "not ready yet..."
             sleep 1
         fi
+        i=$(($i+$j))
     done
 fi
