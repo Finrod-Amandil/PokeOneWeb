18e52f90a67dbe3263b219402ead6d0fa2e621be
AmazingHooman <89647314+AmazingHooman@users.noreply.github.com>
2022-03-18 09:09:45 +0100

    POK-35 angular routing main menu (#98)
    * POK-35 angular routing main menu
    
    * POK-35 angular routing main menu
    
    * POK-35 angular routing main menu
    
    * POK-35 angular routing main menu
    
    * POK-35 angular routing main menu
    
    * POK-35 angular routing main menu
    
    * POK-35 angular routing main menu
    
    Co-authored-by: eggerja1 <eggerja1@students.zhaw.ch>

---
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/pages/about/about.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about.component';
+import { AboutRoutingModule } from './about-routing.module';
+import { ComponentsModule } from 'src/app/core/components/components.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AboutComponent],
+  imports: [
+    CommonModule,
+    AboutRoutingModule,
+    ComponentsModule,
+    FormsModule,
+    RouterModule
+  ]
+})
+export class AboutModule { }
