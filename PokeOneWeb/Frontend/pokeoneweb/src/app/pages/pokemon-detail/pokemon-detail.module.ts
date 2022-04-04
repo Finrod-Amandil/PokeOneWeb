900992c77fa734cc316ad68e846e7adf114059dd
Bischi <rafael.bischofberger@gmail.com>
2022-04-04 17:00:18 +0200

    added material styling for button

--- PokeOneWeb/Frontend/pokeoneweb/src/app/pages/pokemon-detail/pokemon-detail.module.ts
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/pages/pokemon-detail/pokemon-detail.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from'@angular/material/button';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { ComponentsModule } from 'src/app/core/components/components.module';
 import { PokemonDetailRoutingModule } from './pokemon-detail-routing.module';
 import { PokemonDetailComponent } from './pokemon-detail.component';
 
+
 @NgModule({
     declarations: [PokemonDetailComponent],
-    imports: [CommonModule, PokemonDetailRoutingModule, ComponentsModule, NgSelectModule, FormsModule, RouterModule]
+    imports: [CommonModule, PokemonDetailRoutingModule, ComponentsModule, NgSelectModule, FormsModule, RouterModule, MatButtonModule]
 })
 export class PokemonDetailModule {}
