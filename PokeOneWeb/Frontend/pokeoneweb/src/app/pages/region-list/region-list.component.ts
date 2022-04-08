5617c360f0168c431598e528aeb4fdad7fd0d56f
Severin Zahler <zahlesev@students.zhaw.ch>
2022-04-08 16:17:24 +0200

    Stub for region list page

---
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/pages/region-list/region-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pokeone-region-list',
+  templateUrl: './region-list.component.html',
+  styleUrls: ['./region-list.component.scss']
+})
+export class RegionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/pages/region-list/region-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegionListComponent } from './region-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    RegionListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegionListModule { }
