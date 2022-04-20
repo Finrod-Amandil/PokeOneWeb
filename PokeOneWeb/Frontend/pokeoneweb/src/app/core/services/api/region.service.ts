a11f9d4275eb8be1e82a953bb0cb5d898a54cf1c
Finrod_Amandil <severin.zahler@gmail.com>
2022-04-20 21:06:06 +0200

    Change Region Api service to use JSON file

--- PokeOneWeb/Frontend/pokeoneweb/src/app/core/services/api/region.service.ts
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/core/services/api/region.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IRegionListModel } from '../../models/region-list.model';
 import { BaseService } from './base.service';
 
@@ -17,6 +18,6 @@ export class RegionService extends BaseService {
     }
 
     public getAll(): Observable<IRegionListModel[]> {
-        return this.http.get<IRegionListModel[]>(`${this.url}/getall`, this.httpOptions);
+        return this.http.get<IRegionListModel[]>(`${environment.baseUrl}/regions.json`, this.httpOptions);
     }
 }
