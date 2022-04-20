5f2c9b6b8a9a902b9b835507e156946bb03f1bd6
Finrod_Amandil <severin.zahler@gmail.com>
2022-04-20 20:30:34 +0200

    Unit tests for RegionListComponent and RegionListSortService

--- PokeOneWeb/Frontend/pokeoneweb/src/app/pages/region-list/region-list.component.spec.ts
+++ PokeOneWeb/Frontend/pokeoneweb/src/app/pages/region-list/region-list.component.spec.ts
@@ -1,25 +1,79 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RegionListModel } from 'src/app/core/models/region-list.model';
 
 import { RegionListComponent } from './region-list.component';
 
-describe('RegionListComponent', () => {
-  let component: RegionListComponent;
-  let fixture: ComponentFixture<RegionListComponent>;
+describe('Region List Component', () => {
+    let titleService: any;
+    let regionService: any;
+    let sortService: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RegionListComponent ]
-    })
-    .compileComponents();
-  });
+    let component: RegionListComponent;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RegionListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        // Arrange
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        titleService = jasmine.createSpy('Title');
+        regionService = jasmine.createSpy('RegionService');
+        sortService = jasmine.createSpy('RegionListSortService');
+
+        component = new RegionListComponent(titleService, regionService, sortService);
+    });
+
+    describe('getRegionType', () => {
+        it('When only isMainRegion flag is set should return "Main Region"', () => {
+            // Arrange
+            const model = new RegionListModel();
+            model.isMainRegion = true;
+            model.isSideRegion = false;
+            model.isEventRegion = false;
+
+            // Act
+            const result = component.getRegionType(model);
+
+            // Assert
+            expect(result).toBe('Main Region');
+        });
+
+        it('When only isSideRegion flag is set should return "Side Region"', () => {
+            // Arrange
+            const model = new RegionListModel();
+            model.isMainRegion = false;
+            model.isSideRegion = true;
+            model.isEventRegion = false;
+
+            // Act
+            const result = component.getRegionType(model);
+
+            // Assert
+            expect(result).toBe('Side Region');
+        });
+
+        it('When only isEventRegion flag is set should return "Event Region"', () => {
+            // Arrange
+            const model = new RegionListModel();
+            model.isMainRegion = false;
+            model.isSideRegion = false;
+            model.isEventRegion = true;
+
+            // Act
+            const result = component.getRegionType(model);
+
+            // Assert
+            expect(result).toBe('Event Region');
+        });
+
+        it('When all flags are set should return "Main Region"', () => {
+            // Arrange
+            const model = new RegionListModel();
+            model.isMainRegion = true;
+            model.isSideRegion = true;
+            model.isEventRegion = true;
+
+            // Act
+            const result = component.getRegionType(model);
+
+            // Assert
+            expect(result).toBe('Main Region');
+        });
+    });
 });
